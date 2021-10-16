53cf6988336de982c6a99ce5533e84241ae60f10
Alejandro Aravena Lazo <33404111+AlejoArav@users.noreply.github.com>
2021-10-16 12:19:43 -0300

    Update align2D.py

--- forTarget/align2D.py
+++ forTarget/align2D.py
@@ -2,8 +2,8 @@ from modeller import *
 
 env = environ()
 aln = alignment(env)
-mdl = model(env, file='6eqe', model_segment=('FIRST:A','LAST:A'))
-aln.append_model(mdl, align_codes='6eqeA', atom_files='6eqe.pdb')
+mdl = model(env, file='4eam', model_segment=('FIRST:A','LAST:A'))
+aln.append_model(mdl, align_codes='4eamA', atom_files='4eam.pdb')
 aln.append(file='target.fasta', align_codes='target', alignment_format='FASTA')
 aln.align2d()
 aln.write(file='aligned.fasta', alignment_format='FASTA')
